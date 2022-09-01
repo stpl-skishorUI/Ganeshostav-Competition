eea4ad9caef94264f966f083da723df7a523dec1
stpl-somnath <somnath.khabale@shauryatechnosoft.com>
2022-09-01 22:24:31 +0530

    changes in mandal registration

---
+++ src/app/core/services/multiple-file-upload.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+// import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { CommonService } from './common.service';
+import { ErrorsService } from './errors.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MultipleFileUploadService {
+
+  multipleFileArray: any[] = [];
+
+  constructor(private apiService: ApiService,
+    private error: ErrorsService, private commonService: CommonService,) { }
+    // private toastrService: ToastrService
+
+  uploadDocuments(event: any, folderName?: any, allowedDocTypes?: any) {
+
+    let docTypeCheckFlag = true;
+    return new Observable(obj => {
+      const formData = new FormData();
+      if (event.target.files && event.target.files[0]) {
+        var filesAmount = event.target.files.length;
+        for (let i = 0; i < filesAmount; i++) {
+          formData.append("files", event.target.files[i]);
+
+          let nameText = event.target.files[i].name;
+          console.log(nameText,'444')
+          const selResult = nameText.split('.');
+          const docExt = selResult.pop();
+          const docExtLowerCase =  docExt.toLowerCase();
+          if (allowedDocTypes.match(docExtLowerCase)) { }
+          else {
+            docTypeCheckFlag = false;
+          }
+        }
+      }
+
+      if (docTypeCheckFlag == true) {
+        this.apiService.setHttp('post', 'api/UploadDocument/UploadMultipleFiles_v1?FolderName=' + folderName, false, formData, false, 'masterUrl');
+        this.apiService.getHttp().subscribe({
+          next: (res: any) => {
+            if (res.statusCode === "200") {
+              obj.next(res);
+            }
+            else {
+              this.commonService.checkDataType(res.statusMessage) == false ? this.error.handelError(res.statusCode) :''// this.toastrService.error(res.statusMessage);
+            }
+          },
+          error: ((error: any) => {
+            this.error.handelError(error.status);
+          })
+        })
+      }
+      else {
+        obj.error("Only " + allowedDocTypes + " file format allowed.");
+        // this.toastrService.error('Only jpg or png & jpeg file format allowed.');
+      }
+    })
+  }
+}
--- src/app/web/mandal-reigstration/mandal-reigstration.component.ts
+++ src/app/web/mandal-reigstration/mandal-reigstration.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/core/services/api.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
+import { MultipleFileUploadService } from 'src/app/core/services/multiple-file-upload.service';
 
 @Component({
   selector: 'app-mandal-reigstration',
@@ -10,29 +12,29 @@ import { ApiService } from 'src/app/core/services/api.service';
 export class MandalReigstrationComponent implements OnInit {
 
   constructor(
-    private apiService: ApiService, public fb: FormBuilder) { }
+    private apiService: ApiService, public fb: FormBuilder ,public multipleFileUploadSe : MultipleFileUploadService,
+    public error:ErrorsService) { }
   registrationForm: any;
   zpNameArr: any;
+  memberArray: any[] = [];
+  competitionType: any;
+  isSubmmited:boolean = false
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  @ViewChild('fileInput1') fileInput1!: ElementRef;
   competitionTypeArray = [{ "id": 1, "competitionName": "सार्वजनिक गणेशोत्सव स्पर्धा " }, { "id": 2, "competitionName": "घरगुती गौरी सजावट स्पर्धा " }]
   ngOnInit(): void {
     this.defulatForm();
-    this.getZPName()
+    this.getZPName();
   }
 
 
   defulatForm() {
     this.registrationForm = this.fb.group({
-      "createdBy": [''],
-      "modifiedBy": 0,
-      "createdDate": [new Date()],
-      "modifiedDate": [new Date()],
-      "isDeleted": false,
-      "id": 0,
       "competitionTypeId": ['', Validators.required],
       "zpgatId": ['', Validators.required],
       "clientId": ['', Validators.required],
       "villageName": ['', Validators.required],
-      "personName": ['', Validators.required],
+      "personName": ['', ],
       "leadername": [''],
       "leaderMobileNo": [''],
       "vicLeadername": [''],
@@ -40,16 +42,22 @@ export class MandalReigstrationComponent implements OnInit {
       "memberName": [''],
       "memberMobileNo": [''],
       "amount": 0,
-      "paymentScreenPath": ['', Validators.required],
-      "videoPath": ['', Validators.required],
-      "paymentId": ['', Validators.required],
-      "paymentStatus": ['', Validators.required],
-      "remark": ['', Validators.required],
-      "marks": ['', Validators.required],
+      "paymentScreenPath": ['' ],
+      "videoPath": ['',],
+      "paymentId": [''],
+      "paymentStatus": [''],
+      "remark": [''],
+      "marks": [''],
       "moreInfo": ['', Validators.required],
-      "selfPersonName":[''],
-      'selfPersonMobile':[''],
+      "selfPersonName": [''],
+      'selfPersonMobile': [''],
+      "imagePath":[''],
+      "videopath":['']
     })
+    this.competitionType = 1;
+  }
+  get f() {
+    return this.registrationForm.controls;
   }
 
   getZPName() {
@@ -68,9 +76,12 @@ export class MandalReigstrationComponent implements OnInit {
       })
     })
   }
-  memberArray: any[]=[];
+
   addMember() {
-    
+    if (this.f.memberName.status == 'Invalid' || this.f.memberMobileNo.status == 'Invalid') {
+      return;
+    }
+
     let obj = {
       "createdBy": 0,
       "modifiedBy": 0,
@@ -80,19 +91,150 @@ export class MandalReigstrationComponent implements OnInit {
       "id": 0,
       "competitionId": 2,
       "designationId": 3,
-      "personName":this.registrationForm.value.memberName,
-      "mobileNo":this.registrationForm.value.memberMobileNo
+      "personName": this.registrationForm.value.memberName,
+      "mobileNo": this.registrationForm.value.memberMobileNo.toString()
     }
     this.memberArray.push(obj);
-    this.registrationForm.controls['registrationForm'].setValue('');
+    this.registrationForm.controls['memberName'].setValue('');
     this.registrationForm.controls['memberMobileNo'].setValue('');
   }
 
 
-  deleteMember(ind:number,obj:any){
-    this.memberArray.forEach((ele:any,ind)=>{
-      this.memberArray.splice(ind,1);
+  deleteMember(ind: number, obj: any) {
+    if (confirm("Do you want Delete") == true) {
+      this.memberArray.map((ele: any, index: number) => {
+        if (ind == index) {
+          this.memberArray.splice(ind, 1);
+        }
+      })
+    }
+  }
+
+  checkCompetitionType(data: any) {
+    this.competitionType = data.id;
+  }
+
+  submitData() {
+    this.isSubmmited =true;
+    if(this.registrationForm.invalid){
+      return
+    }
+    let formData = this.registrationForm.value;
+    let temp = {
+      "createdBy": 0,
+      "modifiedBy": 0,
+      "createdDate": "2022-09-01T12:46:21.663Z",
+      "modifiedDate": "2022-09-01T12:46:21.663Z",
+      "isDeleted": false,
+      "id": 0,
+      "competitionId": 0,
+      "designationId": 0,
+      "personName": "",
+      "mobileNo": ""
+    }
+    if (this.competitionType == 1) {
+      for (let i = 0; i <= 1; i++) {
+        temp['competitionId'] = 1;
+        temp['designationId'] = i == 0 ? 1 : 2;
+        temp['personName'] = i == 0 ? formData.leadername : formData.vicLeadername;
+        temp['mobileNo'] = i == 0 ? formData.leaderMobileNo.toString() : formData.vicleadermobileNo.toString();
+        this.memberArray.push(temp)
+      }
+    } else {
+      temp['competitionId'] = 2;
+    }
+    let obj = {
+      "createdBy": 0,
+      "modifiedBy": 0,
+      "createdDate": new Date(),
+      "modifiedDate": new Date(),
+      "isDeleted": false,
+      "id": 0,
+      "competitionTypeId": this.competitionType,
+      "zpgatId": formData.zpgatId,
+      "clientId": 0,
+      "villageName": formData.villageName,
+      "personName": this.competitionType == 1 ? formData.personName : formData.selfPersonName,
+      "amount": 0,
+      "paymentScreenPath": "string",
+      "videoPath": this.videopath,
+      "paymentId": "string",
+      "paymentStatus": "string",
+      "remark": "string",
+      "marks": 0,
+      "moreInfo": formData.moreInfo,
+      "competitionMembers": this.memberArray,
+      "mobileNo":"",
+      "compettionImage": this.galleryImagArray
+    }
+    this.apiService.setHttp('post', "api/Competition", false, obj, false, 'masterUrl');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode === "200") {
+          this.registrationForm.reset();        
+        } else {
+
+        }
+      },
+      error: ((error: any) => {
+         this.error.handelError(error.status) 
+      })
     })
-   
+
+  }
+
+
+  galleryImagArray: any[] = [];
+  imageUpload(event: any) { //multiple Image Upload
+    let documentUrl: any = this.multipleFileUploadSe.uploadDocuments(event, "uploads", "png,jpg,jpeg");
+    documentUrl.subscribe({
+      next: (ele: any) => {
+        if (ele.responseData != null) {
+          ele.responseData.forEach((element:any) => {
+            let obj =
+            {
+              "createdBy": 0,
+              "modifiedBy": 0,
+              "createdDate": new Date(),
+              "modifiedDate": new Date(),
+              "isDeleted": false,
+              "id": 0,
+              "competitionId": 0,
+              "imagePath": element.filePath,
+              "isMainImage": true,
+              "isImage": 1
+            }
+            this.galleryImagArray.push(obj);
+          });
+        }
+      },
+    })
+    this.registrationForm.controls['imagePath'].setValue('');
+  }
+
+  deleteImage(index: any) {
+    this.galleryImagArray.splice(index, 1);
+    this.fileInput.nativeElement.value = '';
+  }
+
+  videopath:any;
+  videoPath(event: any) { //multiple Image Upload
+    let documentUrl: any = this.multipleFileUploadSe.uploadDocuments(event, "uploads", "mp4,FLV,F4V,AVI,MKV");
+    documentUrl.subscribe({
+      next: (ele: any) => {
+        if (ele.responseData != null) {
+          ele.responseData.forEach((element:any) => {            
+            this.videopath =element.filePath;
+            
+          });
+        }
+      },
+    })
+    this.registrationForm.controls['videopath'].setValue('');
+  }
+
+  deleteVideo() {
+    this.videopath=''
+    this.fileInput1.nativeElement.value = '';
   }
 }
