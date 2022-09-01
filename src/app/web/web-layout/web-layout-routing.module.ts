4cc0ea2d7c97e5434dbba57f6ef8873221f7a43c
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-09-01 15:29:35 +0530

    registration routing added

--- src/app/web/web-layout/web-layout-routing.module.ts
+++ src/app/web/web-layout/web-layout-routing.module.ts
@@ -4,9 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('../home/home.module').then(m => m.HomeModule),data: { title: ' Home' } },
-  { path: 'login', loadChildren: () => import('../login/login.module').then(m => m.LoginModule), data: { title: 'Login' }, },
-  
-  { path: 'mandal-reigstration', loadChildren: () => import('../mandal-reigstration/mandal-reigstration.module').then(m => m.MandalReigstrationModule) },
+  { path: 'login', loadChildren: () => import('../login/login.module').then(m => m.LoginModule), data: { title: 'Login' } },
+  { path: 'registrations', loadChildren: () => import('../mandal-reigstration/mandal-reigstration.module').then(m => m.MandalReigstrationModule), data: { title: 'Registrations' } },
 ];
 
 @NgModule({
