5ff0b48a75a0a363052525d5611d2b454d63e737
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2022-09-03 16:46:42 +0530

    commit

--- src/app/partial/dashboard/dashboard.component.ts
+++ src/app/partial/dashboard/dashboard.component.ts
@@ -22,7 +22,6 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCompetitionType();
-    this.defultForm()
   }
 
   getCompetitionType() {
@@ -40,50 +39,5 @@ export class DashboardComponent implements OnInit {
       })
     })
   }
-
-  remarksForm: any;
-  isSubmmited: boolean = false;
-  defultForm() {
-    this.remarksForm = this.fb.group({
-      marks: ['', [Validators.required]],
-      remarks: ['', [Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)]],
-    })
-  }
-  get f() {
-    return this.remarksForm.controls;
-  }
-
-  addMarks() {
-    this.isSubmmited = true;
-    let formData = this.remarksForm.value;
-    if (this.remarksForm.invalid) {
-      ((formData.marks == 0)) ? this.commonService.showError("Please Enter Valid Marks"):''
-      return;
-    }
-   
-    this.spinner.show();
-    let data = "CompetitionId=" + 32 + "&Marks=" + formData.marks + "&Remark=" + formData.remarks;
-    this.apiService.setHttp('post', "api/Competition/UpdateMarks?" + data, false, false, false, 'masterUrl');
-    this.apiService.getHttp().subscribe({
-      next: (res: any) => {
-        if (res.statusCode === "200") {
-          this.spinner.hide();
-          this.resetForm();
-          this.commonService.showSuccess(res.statusMessage);
-        } else {
-          this.spinner.hide();         
-          this.commonService.showError(res.statusMessage);
-        }
-      },
-      error: ((error: any) => {
-        this.error.handelError(error.status)
-      })
-    })
-  }
-
-resetForm(){
-this.remarksForm.reset();
-this.isSubmmited = false;
-}
 
 }
